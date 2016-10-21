5334142c799a0b5c08e8e1c1803c616723620329
wukezhan <wukezhan@gmail.com>
2016-10-21 14:49:16 +0800

    .

--- tests/travis/run-tests.sh
+++ tests/travis/run-tests.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 TEST_DIR="`pwd`/tests/"
 
-make test 
-
 for file in `find $TEST_DIR -name "*.diff" 2>/dev/null`
 do
-    grep "\-\-XFAIL--" ${file/%diff/phpt} >/dev/null 2>&1
-    if [ $? -gt 0 ]
-    then
-        FAILS[${#FAILS[@]}]="$file"
-    fi
+        FAILS[${#FAILS[@]}]=${file%%diff*}
 done
 
 if [ ${#FAILS[@]} -gt 0 ]
 then
     for fail in "${FAILS[@]}"
     do
-        sh -xc "cat $fail"
+        echo "========$fail.phpt========"
+        echo "--------$fail.diff--------"
+        sh -xc "cat $fail.diff"
+        echo "--------$fail.out --------"
+        sh -xc "cat $fail.out"
+        echo "########$fail.phpt########"
     done
     exit 1
 else
